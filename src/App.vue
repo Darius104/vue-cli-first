af87b67148021e6e4b95ce747323b39ea4edb6fd
Darius104 <comandarius@gmail.com>
2021-12-17 16:37:15 +0100

    Last Push

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Title />
+    <Img />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Title from "./components/Title.vue";
+import img from "./components/Hero.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Title,
+    img,
   },
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  // font-family: Avenir, Helvetica, Arial, sans-serif;
+  // -webkit-font-smoothing: antialiased;
+  // -moz-osx-font-smoothing: grayscale;
+  // text-align: center;
+  // color: #2c3e50;
+  // margin-top: 60px;
+  
+  
+  //reset
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 }
 </style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <img src="https://images.everyeye.it/img-notizie/discord-acquisto-microsoft-punta-quotazione-borsa-v4-512546-640x360.webp" alt="Discord">
+    </div>
+</template>
+
+<script>
+export default{
+    img: "Banner",
+}
+</script>
+
+<style scoped>
+    img{
+        width: 200px;
+        height: 200px;
+    }
+</style>
+
